c0f2c5abf4c597449faf55d1111ca2631de90624
Ziga Cernigoj <ziga.cernigoj@sinergise.com>
2019-05-13 23:51:20 +0200

    add gitignore, functions for getting raw data from files

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# inverted-index
+# Inverted index: building it and using it
+
+## Requirements
+
+### Python
+
+Download Python 3.6 or 3.7 from [Python website](https://www.python.org/downloads/). 
+We don't guarantee that all libraries work with lower 3.x sub-versions. 
+Do not use Python 2.7.
+
+#### Note
+All instructions are tested on Linux, we don't guarantee that everything will work on Windows or MacOS.
+All further instructions are written with assumption that Python 3 is your default Python 
+(be sure to check the PATH or write `python3` and `pip3` instead of `python` and `pip` ).
+
+In case of `EnvironmentError`, run `pip install <module> --user`.
+Or you can use pipenv. 
+You need to install it first (`pip install pipenv`) and then run `pipenv shell`. 
+Note that you must then install needed libraries with `pipenv` not `pip`.
+
+#### Libraries
+
+- BeautifulSoup4: run `pip install bs4`
+
---
+++ indexer/data_processor.py
@@ -0,0 +1,61 @@
+# DATA PROCESSOR:
+# read HTML files
+# get text from HTML files
+
+import os
+from os.path import isfile
+from bs4 import BeautifulSoup
+
+
+# returns only a text from HTML document
+def get_text(filepath):
+    if not isfile(filepath):
+        return None
+
+    print(filepath)
+    f = open(filepath, "r", encoding='utf-8', errors='ignore')
+    content = f.read()
+    f.close()
+
+    parsed = BeautifulSoup(content, 'html.parser')
+    for s in parsed("script"):
+        s.decompose()
+    for s in parsed("noscript"):
+        s.decompose()
+    for s in parsed("style"):
+        s.decompose()
+
+    return parsed.text
+
+
+# returns a list of all filepaths
+def get_filepaths():
+    base_dir = "./PA3-data"
+    filepath_list = []
+
+    for root, dirs, files in os.walk(base_dir):
+        filepath_list.extend(os.path.join(root, x) for x in files if x.endswith(".html"))
+
+    return filepath_list
+
+
+# just a demo, how to call functions above
+def main():
+
+    paths = get_filepaths()
+
+    for p in paths:
+        get_text(p)
+
+    pass
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
