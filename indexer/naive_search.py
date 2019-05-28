5a290e6b75a433ba734f9e948b6f8f65768b8fcf
Ziga Cernigoj <ziga.cernigoj@sinergise.com>
2019-05-29 00:13:35 +0200

    naive search

--- indexer/naive_search.py
+++ indexer/naive_search.py
@@ -24,7 +24,7 @@ def main():
     slovene_stopwords = get_stopwords()
 
     # process the query
-    text_for_query = get_text(query1)
+    text_for_query = get_text(query3)
     all_tokens_for_query = nltk.word_tokenize(text_for_query)
 
     print("q1 text", text_for_query)
@@ -32,31 +32,59 @@ def main():
 
     print("Results for a query: \"", text_for_query, "\"", "\n")
 
-    print("Frequencies Document                                  Snippet")
-    print("----------- ----------------------------------------- -----------------------------------------------------------")
-
+    results = []
     for filePath in paths:
         original_text_for_doc = get_text(filePath)
         all_tokens_for_doc = nltk.word_tokenize(original_text_for_doc)
         postings_for_doc = get_postings(all_tokens_for_doc, slovene_stopwords)
 
-        if all_tokens_for_query[0] in postings_for_doc:
+        all_snippets = ""
+        frequencies = 0
 
-            snippet = ""
-            for index in postings_for_doc[all_tokens_for_query[0]]["indexes"]:
-                snippet = snippet + "..." \
-                                    + all_tokens_for_doc[index-3] + " " \
-                                    + all_tokens_for_doc[index-2] + " " \
-                                    + all_tokens_for_doc[index-1] + " " \
-                                    + all_tokens_for_doc[index] + " " \
-                                    + all_tokens_for_doc[index+1] + " " \
-                                    + all_tokens_for_doc[index+2] + " " \
-                                    + all_tokens_for_doc[index+3] + "..."
+        for word in all_tokens_for_query:
+            if word in postings_for_doc:
+                frequencies += postings_for_doc[word]["frequency"]
 
-            print(postings_for_doc[all_tokens_for_query[0]]["frequency"], "\t", filePath, "\t", snippet)
+                snippet = ""
+                for index in postings_for_doc[word]["indexes"]:
+                    if index > 3:
+                        snippet += "... "
+                    if index > 2:
+                        snippet = snippet + all_tokens_for_doc[index-3] + " "
+                    if index > 1:
+                        snippet = snippet + all_tokens_for_doc[index-2] + " "
+                    if index > 0:
+                        snippet += all_tokens_for_doc[index-1] + " "
+
+                    snippet += all_tokens_for_doc[index] + " "
+
+                    if index < len(all_tokens_for_doc) - 1:
+                        snippet += all_tokens_for_doc[index+1] + " "
+                    if index < len(all_tokens_for_doc) - 2:
+                        snippet = snippet + all_tokens_for_doc[index+2] + " "
+                    if index < len(all_tokens_for_doc) - 3:
+                        snippet = snippet + all_tokens_for_doc[index+3] + " "
+                    if index < len(all_tokens_for_doc) - 4:
+                        snippet += "... "
+
+                all_snippets += snippet
+
+        if frequencies > 0:
+            results.append({"frequency": frequencies, "snippets": all_snippets[0:200], "path": filePath})
 
     end = time.time()
-    print("Results found in", end - start)
+    print("Results found in", end - start, "\n")
+
+    print("Frequencies Document                                  Snippet")
+    print(
+        "----------- ----------------------------------------- -----------------------------------------------------------")
+
+    results_sorted = sorted(results, key=lambda x: x["frequency"], reverse=True)
+
+    for res in results_sorted:
+        print(res["frequency"], "\t", res["path"], "\t", res["snippets"])
+
+
 
 if __name__ == "__main__":
     main()
